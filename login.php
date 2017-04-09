64697e2fea874c55443795e44bd8fe57a85a05a1
mansimarkaur <mansimarkaur.mks@gmail.com>
2017-04-10 03:16:32 +0530

    UI changes

--- login.php
+++ login.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: troot
- * Date: 1/2/15
- * Time: 10:05 PM
- */
 
 require('inc/dbPlayer.php');
 require('inc/sessionManager.php');
@@ -76,10 +70,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
 
-    <title>HMS</title>
+    <title>Hostel Management System</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="./dist/css/bootstrap.min.css" rel="stylesheet">
@@ -99,12 +91,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <!-- Custom Fonts -->
     <link href="./dist/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="./dist/css/appStyle.css" rel="stylesheet" type="text/css">
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 
 </head>
 
@@ -114,16 +100,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <div class="container">
     <div class="row">
 
-        <div class="col-md-4 col-md-offset-4">
+        <div class="col-md-6 col-md-offset-3">
 
             <div class="login-panel panel panel-default">
 
                 <div class="panel-heading">
                     <div class="row">
-                        <div class="col-md-2 col-sm-2 col-xs-2">
+                        <div class="col-md-4 col-sm-2 col-xs-2">
                            <a href="index.html" target="_self"><img src="./dist/images/logo.png" alt="Logo"></a>
                         </div>
-                        <div class="col-md-10 col-sm-12 col-xs-10">
+                        <div class="col-md-8 col-sm-12 col-xs-10">
                             <h4 class="pTitle">Hostel Management System</h4>
                             </div>
                     </div>
